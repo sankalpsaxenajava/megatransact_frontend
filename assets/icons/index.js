8e2faf8285dd838e4f44d2580ef0c45ec37fa8d9
Jolin <jche521@aucklanduni.ac.nz>
2024-06-13 13:07:57 +1200

    add country flag icons

--- assets/icons/index.js
+++ assets/icons/index.js
@@ -2,6 +2,9 @@ const icons = {
   userIcon: require('./user.png'),
   scanIcon: require('./scan.png'),
   dataIcon: require('./data.png'),
+  nzIcon: require('./new-zealand.png'),
+  africaIcon: require('./south-africa.png'),
+  usIcon: require('./united-states.png'),
 };
 
 export default icons;

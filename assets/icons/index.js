0f47de4af68e3527a9d65cf87393a50d1bb024dc
Jolin <jche521@aucklanduni.ac.nz>
2024-06-26 11:09:22 +1200

    rename icons and streamline export to index.js

--- assets/icons/index.js
+++ assets/icons/index.js
@@ -9,4 +9,31 @@ const icons = {
   logo: require('./../logos/MegaTransactLogo.png'),
 };
 
-export default icons;
+const interfaceIcons = {
+  arrowDownIcon: require('./arrow_down.png'),
+  arrowLeftIcon: require('./arrow_left.png'),
+  arrowLeftWhiteIcon: require('./arrow_left_white.png'),
+  arrowRightIcon: require('./arrow_right.png'),
+  avatarIcon: require('./avatar.png'),
+  avatarWhiteIcon: require('./avatar_white.png'),
+  dataWhiteIcon: require('./data_white.png'),
+  dataIcon: require('/data.png'),
+  crossIcon: require('./cross.png'),
+  bagIcon: require('./bag.png'),
+  emailIcon: require('./mail.png'),
+  eyeOnIcon: require('./eye_on.png'),
+  eyeOffIcon: require('./eye_off.png'),
+  fingerPrintIcon: require('./fingerprint.png'),
+  phoneIcon: require('./phone.png'),
+  scanWhiteIcon: require('./scan_white.png'),
+  shareIcon: require('./share.png'),
+  downloadIcon: require('./download.png'),
+};
+
+const countryIcons = {
+  nzIcon: require('./nz.png'),
+  africaIcon: require('./south-africa.png'),
+  usIcon: require('./usa.png'),
+};
+
+export {interfaceIcons, countryIcons};
